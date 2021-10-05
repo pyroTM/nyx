95ef41f176ebf294b7468a0a8a6b84ac934ce833
pyro <trisanth9@gmail.com>
2021-10-05 06:36:11 -0500

    adding getuuid

--- nyx.py
+++ nyx.py
@@ -28,9 +28,22 @@ async def getskin(ctx, username):
                          description=f"The skin of the player {username}",
                          color=0x00ff00)
     skin.add_field(value=f"[Download](https://mc-heads.net/download/{username})", name=f"{username}")
-    skin.set_thumbnail(url=f"https://craftar.com/avatars/{username}")
+    skin.set_thumbnail(url=f"https://minotar.net/avatar/{username}")
     skin.set_footer(text="Nyx", icon_url="https://i.ibb.co/Jz2wrk3/4866cf9f0e65da8b94f7eb687df08070.jpg")
     await ctx.send(embed=skin)
+
+
+@nyx.command()
+async def getuuid(ctx, username):
+    url = f"https://api.mojang.com/users/profiles/minecraft/{username}"
+    data = getInfo(url)
+    uuid = data["id"]
+
+    getid = discord.Embed(title=f"{username} to UUID")
+    getid.add_field(name=f"UUID of Player: {username}", value=f"{uuid}")
+    getid.set_thumbnail(url=f"https://minotar.net/avatar/{username}")
+    getid.set_footer(text="Nyx", icon_url="https://i.ibb.co/Jz2wrk3/4866cf9f0e65da8b94f7eb687df08070.jpg")
+    await ctx.send(embed=getid)
 
 
 nyx.run("")
